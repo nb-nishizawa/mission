b6c4165e86decec39a9e39e457281477066cf1eb
nb-nishizawa <nb-nishizawa@domain.fake>
2014-11-19 16:11:32 +0900

    下準備

---
+++ 2014_3q/Assets/Script/Light/LightBlink.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBlink : MonoBehaviour {
+
+	private float range = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		this.transform.light.intensity = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.transform.light.intensity <= 0 ||
+		    this.transform.light.intensity >= 8){
+			range *= -1;
+		}
+		this.transform.light.intensity += range;
+	}
+}
---
+++ 2014_3q/Assets/Script/Light/RandomSpotLight.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class RandomSpotLight : MonoBehaviour {
+
+	private GameObject spotLight;
+	private List<GameObject> lightList;
+
+	// Use this for initialization
+	void Start () {
+		lightList = new List<GameObject>();
+		spotLight = (GameObject)Resources.Load ("Prefabs/SpotLightPrefab");
+		InvokeRepeating("Create", 1.0f, 0.01f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void Create () {
+		if (lightList.Count < 200) {
+			int x = Random.Range(-12,13);
+			int y = Random.Range(-5,6);
+			foreach (GameObject obj in lightList) {
+				if (obj.transform.position.x == x && obj.transform.position.y == y) continue;
+			}
+			lightList.Add((GameObject)Instantiate(spotLight, new Vector3(x,y,0), Quaternion.identity));
+		} else {
+			CancelInvoke();
+			GameObject light = (GameObject)Instantiate(spotLight, new Vector3(0,0,0), Quaternion.identity);
+//			light.transform
+
+		}
+	}
+}
